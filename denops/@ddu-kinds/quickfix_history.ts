68d622fe4bb3a3602dcddbb27bbdf76a2563accc
GitHub <noreply@github.com>
2023-08-26 09:03:03 +0000

    chore(deps): Update deno dependencies

--- denops/@ddu-kinds/quickfix_history.ts
+++ denops/@ddu-kinds/quickfix_history.ts
@@ -3,8 +3,8 @@ import {
   ActionFlags,
   Actions,
   BaseKind,
-} from "https://deno.land/x/ddu_vim@v3.5.1/types.ts";
-import { batch } from "https://deno.land/x/ddu_vim@v3.5.1/deps.ts";
+} from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
+import { batch } from "https://deno.land/x/ddu_vim@v3.6.0/deps.ts";
 
 export type ActionData = {
   nr: number;
