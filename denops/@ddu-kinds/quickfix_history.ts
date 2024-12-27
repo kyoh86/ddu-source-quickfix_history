16f56e0c407c66faaa2cb3ea40db990ebe5f9a1c
kyoh86 <me@kyoh86.dev>
2024-12-27 10:21:02 +0900

    Update dependencies

--- denops/@ddu-kinds/quickfix_history.ts
+++ denops/@ddu-kinds/quickfix_history.ts
@@ -2,8 +2,8 @@ import {
   type ActionArguments,
   ActionFlags,
   type Actions,
-} from "jsr:@shougo/ddu-vim@~9.2.0/types";
-import { BaseKind } from "jsr:@shougo/ddu-vim@~9.2.0/kind";
+} from "jsr:@shougo/ddu-vim@~9.3.0/types";
+import { BaseKind } from "jsr:@shougo/ddu-vim@~9.3.0/kind";
 import { batch } from "jsr:@denops/std@~7.4.0/batch";
 
 export type ActionData = {
